cc8d91826bb5e84d9ef92c82b978b502b6b14854
Debasmita Bhowmick <dbhowmick035@gmail.com>
2025-09-10 10:57:54 +0530

    updated intro

--- src/Pages/Home/components/Main.js
+++ src/Pages/Home/components/Main.js
@@ -32,7 +32,8 @@ const [user, setUser] = useState(null);
         <div className="text my-5">
             <h1 className="text-5xl font-bold">The Ultimate eBook Store</h1>
             {user ? (<p className="text-2xl font-bold">Welcome to the CodeCamp {user.name}</p>) : ""}
-            <p className="text-2xl my-7 px-1 dark:text-slate-300">CodeBook is the world's most popular and authoritative source for computer science ebooks. Find ratings and access to the newest books digitally.</p>
+            <p className="text-2xl my-7 px-1 dark:text-slate-300">CodeCamp! : A one-stop e-commerce platform for e-books, bringing stories, knowledge, and learning to your fingertips.<br/>
+            Shop smarter, read faster—unlock endless e-books with just a click</p>
             <Link to="/products" type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"> Explore eBooks</Link>
         </div>
         <div className="visual my-5 lg:max-w-xl">
